7951d5af9cbdb9bb1625dabb537e4b4e3d20a9dc
Skrooge Deploy <deploy@skrooge.ai>
2026-09-03 19:47:06 +0200

    Deploy reports demo (variant D + share C)

--- assets/route-block.moVAoSqy.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index.aiq0CXiw.js";const t=Object.freeze(Object.defineProperty({__proto__:null,useAuthStore:e},Symbol.toStringTag,{value:"Module"})),r={};export{t as a,r as b};
--- share/assets/index.Df3dcve7.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index.DLW6aCvD.js";var s={name:"OverlayPanel",extends:e,mounted:function(){console.warn("Deprecated since v4. Use Popover component instead.")}};export{s};
